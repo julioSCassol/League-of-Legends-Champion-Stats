7e5053fee4af49b558f99a42e0032b457e45ade9
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-10-15 23:28:29 -0300

    checkpoint: commit made for save progress to merge with new changes

--- app/src/main/java/com/example/league_of_legends_application/utils/LoadImage.kt
+++ app/src/main/java/com/example/league_of_legends_application/utils/LoadImage.kt
@@ -2,28 +2,13 @@ package com.example.league_of_legends_application.utils
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.foundation.Image
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
-
-@Composable
-fun LoadImageFromUrl(imageUrl: String) {
-    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
-
-    LaunchedEffect(imageUrl) {
-        imageBitmap = loadImageFromUrl(imageUrl)
-    }
-
-    imageBitmap?.let { bitmap ->
-        Image(bitmap = bitmap, contentDescription = "Loaded Image from URL")
-    }
-}
 
 suspend fun loadImageFromUrl(imageUrl: String): ImageBitmap? {
     return withContext(Dispatchers.IO) {
