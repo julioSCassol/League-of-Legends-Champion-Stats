e1a5fb6b4b2e27c3fafd8d40f668c66fd34d50de
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-10-16 13:27:54 -0300

    feat: add push notifications on finish the random teams creation

--- app/src/main/java/com/example/league_of_legends_application/MainActivity.kt
+++ app/src/main/java/com/example/league_of_legends_application/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.league_of_legends_application
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
 import com.example.league_of_legends_application.ui.screens.SplashScreen
 import com.example.league_of_legends_application.ui.screens.AppNavigation
 import com.example.league_of_legends_application.viewmodel.ChampionViewModel
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
@@ -29,19 +25,6 @@ class MainActivity : ComponentActivity() {
             } else {
                 AppNavigation(viewModel)
             }
-        }
-
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w("MainActivity", "Fetching FCM registration token failed", task.exception)
-                return@addOnCompleteListener
-            }
-
-            val token = task.result
-
-            val msg = getString(R.string.msg_token_fmt, token)
-            Log.d("MainActivity", msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
         }
     }
 }
